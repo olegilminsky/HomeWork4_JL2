135f04e2a1a9d44692eaccf253284e90da309cd2
olegilminsky <olegilminsky@yandex.ru>
2021-02-16 15:24:30 +0300

    Здесь ДЗ к 4-му уроку JavaLevel2 (старт потока 2021.01.29)

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -9,13 +9,16 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Online chat");
+        primaryStage.setScene(new Scene(root, 350, 450));
+        primaryStage.setMinWidth(300);
+        primaryStage.setMinHeight(400);
+        primaryStage.setMaxWidth(500);
+        primaryStage.setMaxHeight(600);
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
